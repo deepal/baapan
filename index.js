2e330f036d42189022fa858823de87154462ba68
Deepal Jayasekara <dpjayasekara@gmail.com>
2019-06-30 13:02:37 +0200

    Implement synchronous module loading

--- index.js
+++ index.js
@@ -1,38 +1,117 @@
+#!/usr/bin/env node
+import os from 'os';
+import path from 'path';
+import {mkdirSync, statSync} from 'fs';
+import repl from 'repl';
+import {execSync} from 'child_process'
+import 'colors';
 
-require('babel-polyfill');
-const npm = require('npm');
+const HOME_DIR = os.homedir();
+const WORKSPACE_DIR = '.baapan/workspace';
+const workspacePath = path.join(HOME_DIR, WORKSPACE_DIR);
 
-function parsePath(module) {
-    if (typeof module === 'string') {
-      const [moduleName, ...subDirs] = module.split('/');
+/**
+ * Initialize workspace as an npm project
+ * @param {string} wsPath Workspace Path
+ */
+function initializeWorkspace(wsPath) {
+  process.chdir(wsPath);
+  try {
+    statSync(path.join(wsPath, 'package.json'));
+  } catch (err) {
+    console.log('Initializing workspace...');
+    if (err.code === 'ENOENT') {
+      execSync('npm init -y --scope baapan');
+    }
+  }
+}
+
+/**
+ * Create workspace directory
+ * @param {string} wsPath Workspace path
+ */
+function createWorkspace(wsPath) {
+  mkdirSync(wsPath, {recursive: true});
+  process.chdir(wsPath);
+}
+
+/**
+ * Switch to the workspace directory
+ * @param {string} wsPath Workspace path
+ */
+function switchToWorkspace(wsPath) {
+  try {
+    // Attempt to clean up any existing workspace
+    execSync(`rm -rf ${wsPath}`);
+  } catch (err) {
+    // Do nothing
+  } finally {
+    console.log('Creating workspace...');
+    createWorkspace(wsPath);
+    initializeWorkspace(wsPath);
+    console.log(`Workspace loaded!`);
+  }
+}
+
+/**
+ * Install npm module onto the REPL
+ * @param {string} moduleName Module name
+ */
+function installModule(moduleName) {
+  console.log(`Baapan '${moduleName}' into the workspace!!`);
+  execSync(`npm install --silent --save-exact ${moduleName}`);
+}
+
+/**
+ * Parse module name from the require'd module path
+ * @param {string} moduleStr Module name/path
+ */
+function parseModulePath(moduleStr) {
+    if (typeof moduleStr === 'string') {
+      const tokens = moduleStr.split('/');
+      if (tokens.length && tokens[0].startsWith('@')) {
+        // Scoped npm module. e.g, @babel/register
+        return [tokens.slice(0,2).join('/')];
+      }
+      const [moduleName, ...subDirs] = tokens;
       return [moduleName, subDirs.join('/')];
     }
+
     return [];
 }
 
-global.baapan = async (module) => {
-  const [moduleName] = parsePath(module);
+/**
+ * Load a module or install it an load if not already installed
+ * @param {string} pkgName Module name to require
+ */
+function baapan (pkgName) {
+  const [moduleName] = parseModulePath(pkgName);
 
   if (!moduleName) {
     return console.error('Invalid module name provided!');
   }
 
-  return new Promise((resolve, reject) => {
-    try {
-      resolve(require(module));
-    } catch (err) {
-      npm.load({
-          loaded: false,
-          loglevel: 'silent',
-          force: true,
-          progress: false
-      }, function (err) {
-        if (err) return reject(err);
-        npm.commands.install([moduleName], function (er, [[mName, mPath]]) {
-          if (er) return reject(er);
-          return resolve(require(module));
-        });
-      });
-    }
-  });
+  const absoluteModulePath = path.join(process.cwd(), './node_modules', pkgName);
+  try {
+    require.resolve(absoluteModulePath);
+  } catch (err) {
+    installModule(moduleName);
+  }
+
+  try {
+    return require(absoluteModulePath);
+  } catch (err) {
+    console.log(`Could not load module '${pkgName}'`);
+  }
 }
+
+/**
+ * Start baapan REPL
+ */
+function startRepl() {
+  switchToWorkspace(workspacePath);
+  const replServer = repl.start('> ');
+  replServer.context.baapan = baapan;
+}
+
+startRepl();
--- readme.md
+++ readme.md
@@ -4,8 +4,6 @@
 
 **Baapan brings the all of the NPM goodness right into your REPL. On-demand!!.**
 
-> `baapan` only works on Node.js v10.x.x or above as it requires REPL to be started with `--experimental-repl-await` flag.
-
 Using Node REPL for quick local testing is a common practice among JavaScript/Node developers. But the problem with the REPL is, you don't have the luxury to take advantage of the entire NPM ecosystem out-of-the-box. 
 
 An alternative is, using something like RunKit. But fiddling with sensitive data in an online tool is not the best choice for many developers.
@@ -20,54 +18,38 @@ While using Node REPL, if you feel you need an NPM module to quickly test someth
 
 #### Step 1: Install baapan on your home directory
 
-Navigate to your home directory, and run:
+Simply run:
 
 ```sh
-npm install baapan
+npm install -g baapan
 ```
+This will install `baapan` CLI command.
+
 #### Step 2: Load baapan with REPL
 
 ```sh
-node --require baapan --experimental-repl-await
+$ baapan
 ```
-**Important!!** Baapan relies on `async-await` in REPL, therefore you need to enable the use of `async-await` in REPL using `--experimental-repl-await` flag.
 
-
-#### Step 3: Use `await baapan()` instead of `require`
+#### Step 3: Use `baapan` instead of `require`
 
 ```
-> const lodash = await baapan('lodash')
-+ lodash@4.17.11
-updated 1 package and audited 24204 packages in 2.501s
-found 0 vulnerabilities
-
-> lodash.concat([1], 2, [3,4])
+Creating workspace...
+Initializing workspace...
+Workspace loaded!
+> const lodash = baapan('lodash')
+Baapan 'lodash' into the workspace!!
+undefined
+> lodash.concat([1], [2,3], 4)
 [ 1, 2, 3, 4 ]
 ```
 
-Baapan will immediately download the module via `npm` and `require` it so that it's available on the REPL!!!!
+Baapan will immediately download the module via `npm` and `require` it immediately so that it's available on the REPL!!!!
 
 **You can also `require` a submodule with `baapan()`. See the following:**
 
 ![](https://github.com/dpjayasekara/baapan/raw/master/docs/uuid.png)
 
-------------------
-
-**If you feel lazy to type the entire command everytime you need to start the REPL, just go ahead and add an alias for the command in your `~/.bashrc` or `~/.bash_profile`.**
-
-For `~/.bashrc`
-
-```
-$ echo "alias nodeb='node --require baapan --experimental-repl-await'" >> ~/.bashrc
-$ source ~/.bashrc
-```
-
-For `~/.bash_profile`
-
-```
-$ echo "alias nodeb='node --require baapan --experimental-repl-await'" >> ~/. bash_profile
-$ source ~/.bash_profile
-```
 
 **Feel free to drop any issues/feature requests/PRs at any time!!**
 
