e700a6b9dabface8340ab05aeb01408951dcd153
Deepal Jayasekara <dpjayasekara@gmail.com>
2019-07-07 19:34:19 +0200

    Fix issue #7 and multiple other improvements
    - Workspace directory name is now appended with the process pid and the epoch timestamp in order to allow multiple baapan REPL sessions at once
    - Clean up workspace directory automatically on exit
    - 3rd party modules are now attempted to resolve on the workspace directory first

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 #!/usr/bin/env node
 import os from 'os';
 import path from 'path';
-import {statSync} from 'fs';
+import { statSync } from 'fs';
 import mkdirp from 'mkdirp';
 import repl from 'repl';
-import {execSync} from 'child_process';
+import { execSync } from 'child_process';
 import { Module } from 'module';
 
 const HOME_DIR = os.homedir();
-const WORKSPACE_DIR = '.baapan/workspace';
+const WORKSPACE_DIR = `.baapan/workspace_${process.pid}_${Date.now()}`;
 const workspacePath = path.join(HOME_DIR, WORKSPACE_DIR);
+const workspaceModulesDir = path.join(workspacePath, 'node_modules');
 
 /**
  * Initialize workspace as an npm project
@@ -28,6 +29,14 @@ function initializeWorkspace(wsPath) {
 }
 
 /**
+ * Clean up workspace directory
+ * @param {string} wsPath
+ */
+function cleanUpWorkspace(wsPath) {
+  execSync(`rm -rf ${wsPath}`);
+}
+
+/**
  * Create workspace directory
  * @param {string} wsPath Workspace path
  */
@@ -43,14 +52,14 @@ function createWorkspace(wsPath) {
 function switchToWorkspace(wsPath) {
   try {
     // Attempt to clean up any existing workspace
-    execSync(`rm -rf ${wsPath}`);
+    cleanUpWorkspace(wsPath);
   } catch (err) {
     // Do nothing
   } finally {
     console.log('Creating workspace...');
     createWorkspace(wsPath);
     initializeWorkspace(wsPath);
-    console.log(`Workspace loaded!`);
+    console.log('Workspace loaded!');
   }
 }
 
@@ -59,38 +68,86 @@ function switchToWorkspace(wsPath) {
  * @param {string} moduleName Module name
  */
 function installModule(moduleName) {
-  execSync(`npm install --silent --save-exact ${moduleName}`);
+  execSync(`npm install --silent ${moduleName}`);
 }
 
-/**
- * Parse module name from the require'd module path
- * @param {string} moduleStr Module name/path
- */
-function parseModulePath(moduleStr) {
-    if (typeof moduleStr === 'string') {
-      const tokens = moduleStr.split('/');
-      if (tokens.length && tokens[0].startsWith('@')) {
-        // Scoped npm module. e.g, @babel/register
-        return [tokens.slice(0,2).join('/')];
-      }
-      const [moduleName, ...subDirs] = tokens;
-      return [moduleName, subDirs.join('/')];
+function callerFile() {
+  const orig = Error.prepareStackTrace;
+  Error.prepareStackTrace = function (_, stack) { return stack; }; // eslint-disable-line func-names
+  const err = new Error();
+  Error.captureStackTrace(err);
+  const { stack } = err;
+  Error.prepareStackTrace = orig;
+
+  const currentFile = stack.shift().getFileName();
+  const ignoredCallers = ['internal/modules/cjs'];
+
+  while (stack.length) {
+    const callingFile = stack.shift().getFileName();
+    if (callingFile !== currentFile && !ignoredCallers.includes(path.dirname(callingFile))) {
+      return callingFile;
     }
-
-    return [];
-}
-
-function getAbsoluteModulePath(pkgName) {
-  const [moduleName] = parseModulePath(pkgName);
-
-  if (!moduleName) {
-    return console.error('Invalid module name provided!');
   }
 
-  return {
-    path: path.join(process.cwd(), './node_modules', pkgName),
-    name: moduleName
+  return null;
+}
+
+function isLocalModule(moduleName) {
+  return /^[/.]/.test(moduleName);
+}
+
+function isNativeModule(moduleName) {
+  return Object.prototype.hasOwnProperty.call(
+    process.binding('natives'),
+    moduleName,
+  );
+}
+
+function getModuleAbsPath(modulePath) {
+  const caller = callerFile();
+  return path.join(path.dirname(caller), modulePath);
+}
+
+function getModuleInfo(requiredPath) {
+  let moduleInfo = {
+    path: '',
+    isLocalModule: isLocalModule(requiredPath),
+    isNativeModule: false,
+    isThirdPartyModule: false,
   };
+
+  if (!requiredPath || typeof requiredPath !== 'string') {
+    throw new Error('invalid module path');
+  }
+
+  if (requiredPath.startsWith(path.sep)) {
+    moduleInfo = {
+      ...moduleInfo,
+      path: requiredPath,
+    };
+  } else if (requiredPath.startsWith('.')) {
+    moduleInfo = {
+      ...moduleInfo,
+      path: getModuleAbsPath(requiredPath),
+    };
+  } else if (requiredPath.startsWith('@')) {
+    moduleInfo = {
+      ...moduleInfo,
+      path: requiredPath.split('/').slice(0, 2).join('/'),
+      isThirdPartyModule: true,
+    };
+  } else {
+    const moduleReqPath = requiredPath.split('/')[0];
+    const isNative = isNativeModule(moduleReqPath);
+    moduleInfo = {
+      ...moduleInfo,
+      path: moduleReqPath,
+      isThirdPartyModule: !isNative,
+      isNativeModule: isNative,
+    };
+  }
+
+  return moduleInfo;
 }
 
 /**
@@ -98,7 +155,7 @@ function getAbsoluteModulePath(pkgName) {
  * @deprecated
  * @param {string} pkgName Module name to require
  */
-function baapan (pkgName) {
+function baapan(pkgName) {
   console.warn('use of \'baapan()\' is deprecated! You can now directly use \'require()\' instead. Isn\'t that cool?');
   return require(pkgName);
 }
@@ -108,25 +165,23 @@ function baapan (pkgName) {
  */
 function wrapRequire() {
   const originalRequire = Module.prototype.require;
+  require.resolve.paths = [path.join(workspacePath, 'node_modules')];
+  Module.prototype.require = function (moduleName) {
+    // Inject workspace node_modules directory
+    this.paths.unshift(workspaceModulesDir);
 
-  Module.prototype.require = function(moduleName, ...args) {
-    const isLocalModule = /^[\/\.]/.test(moduleName);
-    const isNativeModule = process.binding("natives").hasOwnProperty(moduleName);
-
-    if (isLocalModule || isNativeModule) {
-      return originalRequire.call(this, moduleName, ...args);
-    }
-
-    const requiredModule = getAbsoluteModulePath(moduleName);
+    const moduleInfo = getModuleInfo(moduleName);
 
     try {
-      require.resolve(requiredModule.path);
+      return originalRequire.apply(this, [moduleName]);
     } catch (err) {
-      // module not found
-      installModule(requiredModule.name);
+      if (!moduleInfo.isThirdPartyModule) throw new Error('attempted module to install appears to be a local or native module');
+      installModule(moduleInfo.path);
+      return originalRequire.apply(this, [
+        path.join(workspacePath, 'node_modules', moduleName),
+      ]);
     }
-    return originalRequire.call(this, requiredModule.path, ...args);
-  }
+  };
 }
 
 /**
@@ -138,5 +193,10 @@ function startRepl() {
   const replServer = repl.start('> ');
   replServer.context.baapan = baapan;
 }
+
+process.on('exit', () => {
+  console.info('Cleaning up workspace...');
+  cleanUpWorkspace(workspacePath);
+});
 
 startRepl();
