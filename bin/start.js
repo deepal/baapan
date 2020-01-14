4dcb294dd1b7bc6650812b0eb6cb5081daff1f73
Shahid Kamal <shahidkamal08@gmail.com>
2020-01-14 23:22:14 +0530

    top level await

--- bin/start.js
+++ bin/start.js
@@ -3,13 +3,10 @@
 const path = require('path');
 
 const libPath = path.resolve(__dirname, '../lib/index.js');
-
 require('child_process').spawn(process.argv.shift(), [libPath], {
   cwd: process.cwd(),
   env: {
-    ...process.env,
     NODE_OPTIONS: '--experimental-repl-await',
   },
-  windowsHide: true,
   stdio: 'inherit',
 });
