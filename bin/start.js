e3e968187efd9892e1f20d22ebffdf3ab4b9354f
Shahid Kamal <shahidkamal08@gmail.com>
2020-01-16 10:12:14 +0530

    fix env issue

--- bin/start.js
+++ bin/start.js
@@ -3,10 +3,13 @@
 const path = require('path');
 
 const libPath = path.resolve(__dirname, '../lib/index.js');
+
 require('child_process').spawn(process.argv.shift(), [libPath], {
   cwd: process.cwd(),
   env: {
+    ...process.env,
     NODE_OPTIONS: '--experimental-repl-await',
   },
+  windowsHide: true,
   stdio: 'inherit',
 });
